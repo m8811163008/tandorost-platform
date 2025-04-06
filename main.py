9b93216177ef5f394e164f3efb729080f60b1041
m8811163008 <m8811163008@gmail.com>
2025-04-06 20:21:38 +0330

    update git ignore

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ app = FastAPI(root_path="/api/v1")
 async def read_root():
     return {"Hello": "World"}
 
-
 # to get a string like this run:
 # openssl rand -hex 32
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
